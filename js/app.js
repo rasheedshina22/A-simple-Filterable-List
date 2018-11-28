337bc4eddab9fcc410e1068fee2054be0cad7126
shina <rasheedshinaopeyemi@gmail.com>
2018-11-28 15:05:10 +0100

    my first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    document.querySelector(".list-group").style.display = 'none';
+}
+
+
+document.getElementById('filter').addEventListener('keyup', filterList);
+
+function filterList(e){
+    document.querySelector(".list-group").style.display = 'block';
+    var input_value = e.target.value.toLowerCase();
+    if(input_value ==''){
+        document.querySelector('.list-group').style.display = 'none';
+    }else{
+        var items = document.querySelectorAll('.list-group-item');
+        Array.from(items).forEach(function(item){
+        var item_value = item.innerHTML.toLowerCase();
+        if(item_value.indexOf(input_value) == -1){
+            item.style.display = 'none';
+        }else{
+            item.style.display = 'block';
+        }
+    });
+
+    }
+}
